7c7df7e5a956fdff6ce853e1f3aca7c41a6d77cf
Abhinash Tummala <>
2023-02-08 23:54:52 -0800

    namesake

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,9 @@ const Blog: React.FC<Props> = (props) => {
   return (
     <Layout>
       <div className="page">
-        <h1>Public Feed</h1>
+        //update the page. Create new components and refacotor exisiting
+        components
+        <h1>Public Feed Post</h1>
         <main>
           {props.feed.map((post) => (
             <div key={post.id} className="post">
